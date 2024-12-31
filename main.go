5c1f0bd5f99750cd3d93e481731e60dc5ca0f500
among5094 <106166621+among5094@users.noreply.github.com>
2024-12-31 14:47:50 +0900

    Add files via upload

---
+++ main.go
@@ -0,0 +1,11 @@
+package main // main 패키지를 선언
+// import "main.go/ch01"
+import "ch01" // ch01 패키지를 import
+// ch01 패키지를 import
+
+// main 함수는 Go 프로그램의 진입점
+func main() {
+
+	ch01.Print() // ch01 패키지의 Print 함수 호출
+
+}
