1d30115b1f252d5f5364619bb146fe91c8251fb3
among5094 <106166621+among5094@users.noreply.github.com>
2025-01-03 16:08:28 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
-package main     // main 패키지를 선언
-import "GO/ch01" // 모듈 이름을 기준으로 ch01 패키지 import
+package main
+
+import (
+	"GO/ch01"
+	"GO/ch02"
+)
 
 // main 함수는 Go 프로그램의 진입점
 func main() {
 
+	print(" ===== CH01 ===== \n")
 	ch01.Print() // ch01 패키지의 Print 함수 호출
 	print("\n\n\n")
 	ch01.UseVar()
+
+	print(" ===== CH02 ===== \n")
+	ch02.UseString()  // ch02_02.go
+	ch02.UseBoolean() // ch02_03.go
+	ch02.UseIf()      // ch02_04_UseIf.go
 
 }
